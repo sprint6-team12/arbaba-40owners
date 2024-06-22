aaab9bc9e1aba67cc8dce5de1bc653f81f02ebda
김제완 <jeeee_in@naver.com>
2024-06-22 13:15:13 +0900

    DD-3-refactor: Component/filter 컴포넌트 분리

--- src/components/filter/filter.tsx
+++ src/components/filter/filter.tsx
@@ -1,9 +1,13 @@
 import { ko } from 'date-fns/locale';
 import { useEffect, useRef, useState } from 'react';
-import DatePicker, { registerLocale } from 'react-datepicker';
+import { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import IconCloseBlack from '@/../public/images/icon-close-black.svg';
 import { LOCATIONS } from '@/constants/data-locations';
+import DateSection from './DateSection';
+import Divider from './Divider';
+import LocationSection from './LocationSection';
+import PriceSection from './PriceSection';
 
 registerLocale('ko', ko);
 
@@ -65,80 +69,26 @@ export default function Filter() {
       <button onClick={handleFilterClick}>버튼</button>
       {isFilterOpen && (
         <div ref={FilterContainerRef}>
-          <div className="relative w-390px px-20px py-24px overflow-auto border border-solid border-gray200 rounded-10px bg-white">
+          <div className="relative w-390px px-20px py-24px overflow-auto border border-solid border-gray20 rounded-10px bg-white">
             <div className="flex items-center justify-between pb-[18px]">
               <p className="font-bold text-[20px]">상세 필터</p>
               <button onClick={() => setIsFilterOpen(false)}>
                 <IconCloseBlack />
               </button>
             </div>
-            <div className="flex flex-col pb-18px gap-16px">
-              <p>위치</p>
-              <div className="p-36px border border-solid rounded-6px grid grid-cols-2 gap-32px overflow-y-scroll h-350px">
-                {LOCATIONS.map((item) => (
-                  <button
-                    key={item}
-                    type="button"
-                    className="text-[14px]"
-                    onClick={() => handleLocationClick(item)}
-                  >
-                    {item}
-                  </button>
-                ))}
-              </div>
-            </div>
-            <div className="flex flex-wrap gap-8px">
-              {selectedLocations.map((location) => (
-                <div
-                  key={location}
-                  className="flex items-center bg-red100 text-custom-orange px-10px py-6px rounded-20px font-bold text-14px"
-                >
-                  {location}
-                  <button
-                    className="ml-2 text-custom-orange"
-                    onClick={() => handleRemoveLocation(location)}
-                  >
-                    ×
-                  </button>
-                </div>
-              ))}
-            </div>
-            <div className="border-solid border-b-2 border-gray200 my-8px" />
-            <div className="flex flex-col pb-18px gap-16px">
-              <p>시작일</p>
-              <DatePicker
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-                dateFormat="yyyy년 MM월 dd일 (eee)"
-                locale="ko"
-                customInput={
-                  <input
-                    type="text"
-                    className="w-full px-4 border border-gray300 rounded-lg cursor-pointer py-16px"
-                    readOnly
-                  />
-                }
-              />
-            </div>
-            <div className="border-solid border-b-2 border-gray200 my-8px" />
-            <div className="flex flex-col gap-16px">
-              <p>금액</p>
-              <div className="flex items-center gap-18px">
-                <div className="relative">
-                  <input
-                    type="text"
-                    placeholder="입력"
-                    className="w-180px py-16px px-20px border border-gray300 rounded-6px pr-12"
-                    value={inputPrice}
-                    onChange={handlePriceChange}
-                  />
-                  <span className="absolute transform -translate-y-1/2 right-4 top-1/2">
-                    원
-                  </span>
-                </div>
-                <p>이상부터</p>
-              </div>
-            </div>
+            <LocationSection
+              locations={LOCATIONS}
+              selectedLocations={selectedLocations}
+              onLocationClick={handleLocationClick}
+              onRemoveLocation={handleRemoveLocation}
+            />
+            <Divider />
+            <DateSection startDate={startDate} setStartDate={setStartDate} />
+            <Divider />
+            <PriceSection
+              inputPrice={inputPrice}
+              onPriceChange={handlePriceChange}
+            />
             <div className="flex justify-between mt-20px">
               <button
                 className="text-custom-orange py-14px rounded-6px w-80px border border-custom-orange font-bold"
