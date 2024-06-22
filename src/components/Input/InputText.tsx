e050d2f6d4e9edf49bf3c1323f70eff878950309
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-06-22 14:29:19 +0900

    ✨feat: Filter Component 추가 (#7)
    * DD-3-feat: Component/filter
    
    * DD-3-feat: Component/filter 달력 기능 추가
    
    * DD-3-style: Component/filter css 수정
    
    * DD-3-feat: Component/filter 선택지역 태그기능 추가
    
    * DD-3-feat: Component/filter 금액 유효성검사 추가
    
    * DD-3-refactor: Component/filter 축약어 변경
    
    * DD-3-refactor: Component/filter 컨벤션 적용
    
    * DD-3-feat: Component/filter 초기화 기능 추가 및 style 수정
    
    * DD-3-chore: update eslint
    
    * DD-3-style: Component/filter global.css 수정
    
    * DD-3-style: Component/filter 닫기이미지 추가
    
    * DD-3-feat: Component/filter 닫기기능 추가
    
    * DD-3-chore: tailwind.config 오타수정
    
    * DD-3-style: Component/filter 날짜인풋창 패딩 변경
    
    * DD-3-refactor: Component/filter push 오류 수정
    
    * ✨feat: Button Component 추가 (#2)
    
    * feat: Button Component 추가
    
    * styles: button hover 추가
    
    * fix: 링크와 단순버튼 분리 및 if 문 변경
    
    * refactor: Button 리팩토링
    
    * DD-3-refactor: Component/filter 리뷰반영
    
    * DD-3-refactor: Component/filter 컨벤션 정리
    
    * DD-3-refactor: data-locations 삭제
    
    * ✨feat: Input Component 추가 (#4)
    
    * DD-5-feat: Input 컴포넌트 제작
    
    * DD-5-refactor: 컴포넌트 선언 방식 변경 및 아이콘 이름 변경
    
    * DD-5-refactor: 아이콘 이름 변경
    
    * DD-3-refactor: Component/filter 컴포넌트 분리
    
    * Revert "✨feat: Filter Component 추가"
    
    ---------
    
    Co-authored-by: JooKangsan <162934516+JooKangsan@users.noreply.github.com>
    Co-authored-by: Jeongmin <154623483+oris8@users.noreply.github.com>

---
+++ src/components/Input/InputText.tsx
@@ -0,0 +1,18 @@
+import { forwardRef } from 'react';
+
+function InputText(
+  { className, ...rest }: React.InputHTMLAttributes<HTMLInputElement>,
+  ref: React.Ref<HTMLInputElement>
+) {
+  return (
+    <input
+      className={`input-base h-full w-full ${className}`}
+      ref={ref}
+      {...rest}
+    />
+  );
+}
+
+forwardRef(InputText).displayName = 'InputText';
+
+export default forwardRef(InputText);
