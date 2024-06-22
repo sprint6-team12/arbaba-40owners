bff06d43d131b656db98ec167ac15e016dd99257
chanhee won <wch2208@gmail.com>
2024-06-23 01:26:48 +0900

    feat: StatusBadge 컴포넌트 추가
    - props로 api의 status를 전달해주면 해당하는 뱃지를 랜더링함

---
+++ src/components/Badge/StatusBadge.tsx
@@ -0,0 +1,37 @@
+type Status = 'pending' | 'accepted' | 'rejected';
+
+interface StatusBadgeProps {
+  status: Status;
+}
+
+function StatusBadge({ status }: StatusBadgeProps) {
+  const statusMap = {
+    pending: {
+      backgroundColor: 'bg-green10',
+      textColor: 'text-green20',
+      text: '대기중',
+    },
+    accepted: {
+      backgroundColor: 'bg-blue10',
+      textColor: 'text-blue20',
+      text: '승인 완료',
+    },
+    rejected: {
+      backgroundColor: 'bg-red10',
+      textColor: 'text-red40',
+      text: '거절',
+    },
+  };
+
+  const { backgroundColor, textColor, text } = statusMap[status];
+
+  return (
+    <span
+      className={`px-10px py-6px rounded-20px font-bold text-14px inline-flex justify-center items-center ${backgroundColor} ${textColor}`}
+    >
+      {text}
+    </span>
+  );
+}
+
+export default StatusBadge;
