6c41915cdbc74e7e88e1dd54f77683bc8ae917b4
eprnf <eprnf21@gmail.com>
2024-06-22 14:00:39 +0900

    DD-9-refactor: 리뷰 반영(타입 수정)

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,11 +2,24 @@ import Button from '@/components/Button/Button';
 import ModalWrapper from '@/components/Modal/ModalWrapper';
 import IconCheck from '/public/images/icon-check.svg';
 
+interface ModalType {
+  className?: string;
+  optionType?: 'confirm' | 'actions';
+  Icon?: React.ElementType;
+  content?: string;
+  onClose?: () => void;
+  onConfirm?: () => void;
+  onCancel?: () => void;
+}
+
 /**
  *
- * @param optionType - confirm(확인버튼) | actions(예, 아니오 버튼)
- * @param Icon - 아이콘 컴포넌트, 기본값 check
- * @param content - 텍스트 컨텐츠
+ * @param {'confirm' | 'actions'} optionType - confirm(확인버튼) | actions(예, 아니오 버튼)
+ * @param {React.ComponentType} Icon - 아이콘 컴포넌트 (기본값: check아이콘)
+ * @param {string} content - 텍스트 컨텐츠 (기본값: 정말 실행하시겠습니까?)
+ * @param {() => void} onClose - useModal 사용 시 자동으로 추가되는 closeModal 함수
+ * @param {() => void} onConfirm - 확인/예 버튼 클릭 시 실행될 함수
+ * @param {() => void} onCancel - 아니오 버튼 클릭 시 실행될 함수 (optionType이 'actions'일 때 필요)
  *
  * @returns
  */
@@ -18,15 +31,7 @@ function Modal({
   onClose = () => {},
   onConfirm = () => {},
   onCancel = () => {},
-}: {
-  className?: string;
-  optionType?: 'confirm' | 'actions';
-  Icon?: React.ElementType;
-  content?: string;
-  onClose?: () => void;
-  onConfirm?: () => void;
-  onCancel?: () => void;
-}) {
+}: ModalType) {
   const handleConfirm = () => {
     onClose();
     onConfirm();
@@ -37,7 +42,7 @@ function Modal({
     onCancel();
   };
 
-  const ButtonsComponent = () =>
+  const ButtonsComponent =
     optionType === 'confirm' ? (
       <Button className="button_medium" onClick={handleConfirm}>
         확인
@@ -47,7 +52,7 @@ function Modal({
         <Button className="button_medium" onClick={handleCancel}>
           아니요
         </Button>
-        <Button className="button_medium_active" onClick={handleConfirm}>
+        <Button className="button_medium_fill" onClick={handleConfirm}>
           예
         </Button>
       </>
@@ -61,7 +66,7 @@ function Modal({
         <Icon className="mx-auto mb-16px" />
         <p className="text-center">{content}</p>
         <div className="flex-center gap-8px mt-24px [&>button]:w-80px [&>button]:p-0">
-          <ButtonsComponent />
+          {ButtonsComponent}
         </div>
       </div>
     </ModalWrapper>
