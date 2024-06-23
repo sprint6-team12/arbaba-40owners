0f89d132e5a9a2b2b49ee65d90a3d58094996e5d
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-06-23 18:27:03 +0900

    DD-8-feat: 파일 오류 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,3 @@
-import Dropdown from '@/components/Dropdown/Dropdown';
-import { LOCATIONS, MENU_CATEGORIES } from '@/constants/data-locations';
-
 export default function Home() {
-  return (
-    <main>
-      <Dropdown options={MENU_CATEGORIES} />
-      <Dropdown options={LOCATIONS} />
-    </main>
-  );
+  return <main></main>;
 }
