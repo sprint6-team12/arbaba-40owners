c22e8e3e66f39f9ebe972959f628f7ae02ddec31
doh-yo <doyo159090@gmail.com>
2024-06-23 17:50:35 +0900

    DD-14-chore: index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,3 @@
-// pages/index.tsx
-
-import Post from '@/components/Post/Post';
-
-const examplePost = {
-  hourlyPay: 15000,
-  startsAt: '2023-07-01T14:00:00Z',
-  closed: false,
-  workhour: 4,
-  name: 'Sample Restaurant',
-  address1: '123 Main St, City, Country',
-  imageUrl: '/public/images/sample-restaurant.jpg',
-  originalHourlyPay: 13000,
-  href: '/posts/sample-restaurant',
-};
-
 export default function Home() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <Post {...examplePost} />
-    </div>
-  );
+  return <main></main>;
 }
