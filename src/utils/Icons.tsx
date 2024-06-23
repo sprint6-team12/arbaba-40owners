c552ff02adb3d906e84ef7d7cfbcf718382866de
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-06-23 20:22:24 +0900

    feat: svg 파일컴포넌트 형식으로 내보내기 설정

---
+++ src/utils/Icons.tsx
@@ -0,0 +1,43 @@
+import ArrowUpBold from '/public/images/icon-arrow-up-bold.svg';
+import Camera from '/public/images/icon-camera.svg';
+import Check from '/public/images/icon-check.svg';
+import Clock from '/public/images/icon-clock.svg';
+import CloseBlack from '/public/images/icon-close-black.svg';
+import CloseOrange from '/public/images/icon-close-orange.svg';
+import Dropdown from '/public/images/icon-dropdown.svg';
+import EllipseBlue from '/public/images/icon-ellipse-blue.svg';
+import EllipseOrange from '/public/images/icon-ellipse-orange.svg';
+import EmptyCircle from '/public/images/icon-empty-circle.svg';
+import EnvelopeSquare from '/public/images/icon-envelope-square.svg';
+import ExclamationMark from '/public/images/icon-exclamation-mark.svg';
+import Eye from '/public/images/icon-eye.svg';
+import EyeOff from '/public/images/icon-eye-off.svg';
+import FacebookSquare from '/public/images/icon-facebook-square.svg';
+import Instagram from '/public/images/icon-instagram.svg';
+import Location from '/public/images/icon-location.svg';
+import Search from '/public/images/icon-search.svg';
+import StatusActive from '/public/images/icon-status-active.svg';
+import StatusInactive from '/public/images/icon-status-inactive.svg';
+
+export {
+  ArrowUpBold,
+  Camera,
+  Check,
+  Clock,
+  CloseBlack,
+  CloseOrange,
+  Dropdown,
+  EllipseBlue,
+  EllipseOrange,
+  EmptyCircle,
+  EnvelopeSquare,
+  ExclamationMark,
+  Eye,
+  EyeOff,
+  FacebookSquare,
+  Instagram,
+  Location,
+  Search,
+  StatusActive,
+  StatusInactive,
+};
