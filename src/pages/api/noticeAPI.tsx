5780b90686b28a13e9650601151b23289b5a7768
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-06-24 03:01:33 +0900

    DD-37-feat: get api 생성완료

--- src/pages/api/noticeAPI.tsx
+++ src/pages/api/noticeAPI.tsx
@@ -1,7 +1,61 @@
+import { axiosInstance } from '@/pages/api/axiosInstance';
+
+interface GetNoticeListData {
+  offset?: number;
+  limit?: number;
+  address?: string;
+  keyword?: string;
+  startsAtGte?: string;
+  hourlyPayGte?: number;
+  sort?: string;
+}
+
+interface GetShopNoticeListData {
+  shops_id: string;
+  offset?: number;
+  limit?: number;
+}
+
+interface GetShopNoticeData {
+  shops_id: string;
+  notice_id: string;
+}
+
 const noticeAPI = {
-  getNoticeList: () => {},
-  getShopNoticeList: () => {},
-  getShopNotice: () => {},
+  getNoticeList: ({
+    offset,
+    limit,
+    address,
+    keyword,
+    startsAtGte,
+    hourlyPayGte,
+    sort,
+  }: GetNoticeListData) => {
+    const params = {
+      offset,
+      limit,
+      address,
+      keyword,
+      startsAtGte,
+      hourlyPayGte,
+      sort,
+    };
+    return axiosInstance.get(`/notices`, { params });
+  },
+  getShopNoticeList: ({ shops_id, offset, limit }: GetShopNoticeListData) => {
+    const params = {
+      offset,
+      limit,
+    };
+    return axiosInstance.get(`/shops/${shops_id}/notices`, {
+      params,
+    });
+  },
+  getShopNotice: ({ shops_id, notice_id }: GetShopNoticeData) => {
+    return axiosInstance.get(`/shops/${shops_id}/notices/${notice_id}`);
+  },
+  post: () => {},
+  put: () => {},
 };
 
 export default noticeAPI;
