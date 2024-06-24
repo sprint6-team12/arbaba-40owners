6237ca86a7883d6be49abf7b61331547409f1528
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-06-24 14:33:35 +0900

    fix: svg 파일 추가 및 import

--- src/utils/Icons.tsx
+++ src/utils/Icons.tsx
@@ -1,12 +1,20 @@
+import NextArrow from '/public/images/icon-arrow-next-pagination.svg';
+import PrevArrow from '/public/images/icon-arrow-prev-pagination.svg';
 import ArrowUpBold from '/public/images/icon-arrow-up-bold.svg';
+import LargeArrow from '/public/images/icon-arrow-up-large.svg';
+import GrayArrow from '/public/images/icon-arrow-up-gary.svg';
+import SmallArrow from '/public/Images/icon-arrow-up-small.svg';
+import RedArrow from '/public/images/icon-arrow-up-red.svg';
 import Camera from '/public/images/icon-camera.svg';
 import Check from '/public/images/icon-check.svg';
 import Clock from '/public/images/icon-clock.svg';
+import ClockActive from '/public/images/icon-clock-inactive.svg';
 import CloseBlack from '/public/images/icon-close-black.svg';
 import CloseOrange from '/public/images/icon-close-orange.svg';
 import Dropdown from '/public/images/icon-dropdown.svg';
 import EllipseBlue from '/public/images/icon-ellipse-blue.svg';
 import EllipseOrange from '/public/images/icon-ellipse-orange.svg';
+import EllipseGray from '/public/images/icon-ellipse-gray.svg';
 import EmptyCircle from '/public/images/icon-empty-circle.svg';
 import EnvelopeSquare from '/public/images/icon-envelope-square.svg';
 import ExclamationMark from '/public/images/icon-exclamation-mark.svg';
@@ -15,6 +23,7 @@ import EyeOff from '/public/images/icon-eye-off.svg';
 import FacebookSquare from '/public/images/icon-facebook-square.svg';
 import Instagram from '/public/images/icon-instagram.svg';
 import Location from '/public/images/icon-location.svg';
+import LocationActive from '/public/images/icon-location_inactive.svg';
 import Search from '/public/images/icon-search.svg';
 import StatusActive from '/public/images/icon-status-active.svg';
 import StatusInactive from '/public/images/icon-status-inactive.svg';
@@ -24,20 +33,29 @@ export {
   Camera,
   Check,
   Clock,
+  ClockActive,
   CloseBlack,
   CloseOrange,
   Dropdown,
   EllipseBlue,
   EllipseOrange,
+  EllipseGray,
   EmptyCircle,
   EnvelopeSquare,
   ExclamationMark,
   Eye,
   EyeOff,
   FacebookSquare,
+  GrayArrow,
   Instagram,
+  LargeArrow,
   Location,
+  LocationActive,
+  NextArrow,
+  PrevArrow,
+  RedArrow,
   Search,
+  SmallArrow,
   StatusActive,
   StatusInactive,
 };
