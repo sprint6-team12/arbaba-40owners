422093bb2e512de9e37530eb519a3dd7d4c8170f
wch2208 <97877328+wch2208@users.noreply.github.com>
2024-06-24 21:05:09 +0900

    ✨feat: Badge 컴포넌트 추가 (#19)
    * refactor: 반응형 범위 수정
    
    * feat: StatusBadge 컴포넌트 추가
    
    - props로 api의 status를 전달해주면 해당하는 뱃지를 랜더링함
    
    * refactor: StatusBadge 반응형 크기 변경 추가
    
    * feat: FilterBadge 컴포넌트 추가
    
    * feat: RateBadge 컴포넌트 추가
    
    - <RateBadge rate={num} /> 사용 방법
    - rate prop으로 숫자를 넣어주면 글자에 해당 숫자가 나타나고 숫자에 맞춰서 색이 변경됨
    
    * refactor: StatusBadge font weight 수정
    
    * docs: 아이콘 이름 변경
    
    * DD-12-refactor: 상수 분리
    
    * DD-12-refactor: Badge 컴포넌트 피드백 반영
    
    FilterBadge
    - 버튼으로 감싸고 버튼 기본 기능을 확장해서 적용
    RateBadge
    - useColorByRate 훅으로 분리하여 사용
    - 훅의 코드도 피드백 반영하여 상수, 구조분해 적용
    StatusBadge
    - 상수 분리

---
+++ src/components/Badge/StatusBadge.tsx
@@ -0,0 +1,20 @@
+import { STATUS_MAP } from '@/constants/statusMap';
+
+type Status = 'pending' | 'accepted' | 'rejected';
+interface StatusBadgeProps {
+  status: Status;
+}
+
+function StatusBadge({ status }: StatusBadgeProps) {
+  const { backgroundColor, textColor, text } = STATUS_MAP[status];
+
+  return (
+    <span
+      className={`px-10px py-6px rounded-20px tablet:font-bold pc:font-bold text-12px tablet:text-14px pc:text-14px inline-flex justify-center items-center ${backgroundColor} ${textColor}`}
+    >
+      {text}
+    </span>
+  );
+}
+
+export default StatusBadge;
