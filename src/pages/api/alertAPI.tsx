7b775c631e48d38acbdbdd0b2d1854f05b6e4403
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-06-24 16:48:02 +0900

    DD-37-feat: AlertAPI 로직 추가

--- src/pages/api/alertAPI.tsx
+++ src/pages/api/alertAPI.tsx
@@ -1,1 +1,46 @@
 import { axiosInstance } from './axiosInstance';
+
+interface GetAlertData {
+  user_id: string;
+  token?: string | null;
+  offset?: number;
+  limit?: number;
+  alert_id?: string;
+}
+
+const alertAPI = {
+  getAlerts: ({
+    user_id,
+    token = localStorage.getItem('token'),
+    offset,
+    limit,
+  }: GetAlertData) => {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    const params = {
+      offset,
+      limit,
+    };
+    return axiosInstance.get(`/users/${user_id}/alerts`, { params, headers });
+  },
+  putAlerts: ({
+    user_id,
+    token = localStorage.getItem('token'),
+    alert_id,
+  }: GetAlertData) => {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    const requestBody = {};
+    return axiosInstance.put(
+      `/users/${user_id}/alerts/${alert_id}`,
+      requestBody,
+      {
+        headers,
+      }
+    );
+  },
+};
+
+export default alertAPI;
