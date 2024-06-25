24b60e80f9f0d07691837ce3790dd0287edcc0be
Jeongmin <154623483+oris8@users.noreply.github.com>
2024-06-25 14:18:37 +0900

    ✨feat: FormatUtils 추가 (#26)

--- src/lib/utils/NotificationModal.tsx
+++ src/lib/utils/NotificationModal.tsx
@@ -1,15 +1,6 @@
-import { formatDistanceToNow, format, parseISO } from 'date-fns';
-import { ko } from 'date-fns/locale';
 import IconEllipseBlue from '@/../public/images/icon-ellipse-blue.svg';
 import IconEllipseGray from '@/../public/images/icon-ellipse-gray.svg';
 import IconEllipseOrange from '@/../public/images/icon-ellipse-orange.svg';
-
-export const formatDate = (dateString: string) => {
-  return formatDistanceToNow(parseISO(dateString), {
-    addSuffix: true,
-    locale: ko,
-  });
-};
 
 export const getResultInfo = (result: string) => {
   switch (result) {
@@ -31,15 +22,4 @@ export const getResultInfo = (result: string) => {
     default:
       return null;
   }
-};
-
-export const getWorkTime = (startsAt: string, workhour: number) => {
-  const startTime = parseISO(startsAt);
-  const endTime = new Date(startTime.getTime() + workhour * 60 * 60 * 1000);
-  const formattedStartTime = format(startTime, 'yyyy-MM-dd HH:mm', {
-    locale: ko,
-  });
-  const formattedEndTime = format(endTime, 'HH:mm', { locale: ko });
-
-  return { formattedStartTime, formattedEndTime };
 };
