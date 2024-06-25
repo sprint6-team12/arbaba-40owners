5bfb39054f3c464061437ea45de440a595073a12
eprnf <eprnf21@gmail.com>
2024-06-25 14:15:09 +0900

    feat: FormatUtils 추가

--- src/lib/utils/percentageCalculator.ts
+++
@@ -1,9 +0,0 @@
-export default function percentageCalculator(
-  hourlyPay: number,
-  originalHourlyPay: number
-) {
-  const percentage =
-    ((hourlyPay - originalHourlyPay) / originalHourlyPay) * 100;
-  const roundedPercentage = Math.round(percentage / 10) * 10;
-  return roundedPercentage;
-}
