716d58b73370c0620aeb49457245c7b5c245bcae
chanhee won <wch2208@gmail.com>
2024-06-25 13:43:20 +0900

    DD-43-feat: '/'경로 페이지에 Gnb 연결
    - 라우터 로직만 추가한 상태

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { RecoilRoot } from 'recoil';
+import Gnb from '@/components/Gnb/Gnb';
 import ModalsWrapper from '@/components/Modal/ModalsWrapper';
 import PopupsWrapper from '@/components/Popup/PopupsWrapper';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const handleGnbButtonsClick = (pathname: string | undefined) => {
+    if (pathname) {
+      router.push(pathname);
+    }
+  };
   return (
     <RecoilRoot>
       <ModalsWrapper />
       <PopupsWrapper />
+      <Gnb onClick={handleGnbButtonsClick} />
       <Component {...pageProps} />
     </RecoilRoot>
   );
