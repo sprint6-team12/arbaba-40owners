54307a20f6267e5327f2364e632c7b463b991f7f
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-06-26 02:46:51 +0900

    DD-8-refactor: pr내용 반영 수정

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -6,13 +6,15 @@ interface DropdownProps {
   onSelect: (value: string) => void;
   width?: string;
   defaultValue?: string;
-  title?: string;
+  placeholder?: string;
 }
 
 function Dropdown({
   options,
-  title = '타이틀',
+  onSelect,
+  width = '300px',
   defaultValue = '선택',
+  placeholder = '선택해주세요',
 }: DropdownProps) {
   const dropDownClickRef = useRef<HTMLDivElement>(null);
   const [isOpen, setIsOpen] = useState(false);
@@ -25,6 +27,7 @@ function Dropdown({
   const handleSelectOption = (option: string) => {
     setSelectedOption(option);
     setIsOpen(false);
+    onSelect(option); // 선택된 옵션을 상위 컴포넌트로 전달
   };
 
   const handleClick = (e: MouseEvent) => {
@@ -42,22 +45,22 @@ function Dropdown({
       document.removeEventListener('mousedown', handleClick);
     };
   }, []);
+
   return (
     <div className="flex flex-col gap-2 text-black">
-      {title && <p className="text-base leading-6">{title}</p>}
       <div
-        className="relative inline-block w-300px "
+        className={`relative inline-block ${width}`}
         ref={dropDownClickRef}
         onClick={handleToggleDropdown}
       >
         <div className="absolute top-1/2 right-16px transform -translate-y-1/2 w-16px h-16px flex-shrink-0">
           <DropDown />
         </div>
-        <button className="flex w-full px-20px py-16px items-center border bg-white border-gray-30 rounded-md text-base">
-          {selectedOption}
+        <button className="flex w-full px-20px py-16px items-center border bg-white border-gray30 rounded-md text-base">
+          {selectedOption || placeholder}
         </button>
         {isOpen && (
-          <div className="absolute inline-flex flex-col rounded-md border-2px items-center border-gray-20 bg-white shadow-md z-5 mt-8px overflow-y-auto overflow-x-hidden max-h-200px z-dropdown right-7px left-7px">
+          <div className="absolute inline-flex flex-col rounded-md border-2px items-center border-gray20 bg-white shadow-md z-5 mt-8px overflow-y-auto overflow-x-hidden max-h-200px z-dropdown right-7px left-7px">
             {options.map((option) => (
               <div
                 className="w-full flex px-24px py-12px items-center gap-16px cursor-pointer text-base leading-26 tracking--0.16 hover:bg-gray-100 border border-gary-20"
