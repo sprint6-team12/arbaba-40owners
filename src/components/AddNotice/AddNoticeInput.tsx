54c77a29e8188781cca9a0555815a21dfc009688
doh-yo <doyo159090@gmail.com>
2024-06-25 15:03:31 +0900

    DD-33-feat: 폼 유효성 검사로직 및 스타일 수정

--- src/components/AddNotice/AddNoticeInput.tsx
+++ src/components/AddNotice/AddNoticeInput.tsx
@@ -1,79 +1,140 @@
 import { useState, FormEvent } from 'react';
-import FormGroup from '../FormGroup/FormGroup';
 import Button from '../Button/Button';
+import FormGroup from '../FormGroup/FormGroup';
+
+interface FormData {
+  hourlyPay: string;
+  startsAt: string;
+  workHour: string;
+  description: string;
+}
+
+interface FormErrors {
+  hourlyPay: string | null;
+  startsAt: string | null;
+  workHour: string | null;
+}
 
 export default function AddNoticeInput() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [data, setData] = useState({});
+  const [data, setData] = useState<FormData>({
+    hourlyPay: '',
+    startsAt: '',
+    workHour: '',
+    description: '',
+  });
+  const [errors, setErrors] = useState<FormErrors>({
+    hourlyPay: null,
+    startsAt: null,
+    workHour: null,
+  });
 
-  const handleData = (event: any) => {
-    if (event.type === 'click') {
-      const target = event.target as HTMLButtonElement;
-      setData((prev) => ({
-        ...prev,
-        [target.name]: target.textContent,
-      }));
-    } else if (event.type === 'change') {
-      const target = event.target as HTMLInputElement | HTMLTextAreaElement;
-      setData((prev) => ({
-        ...prev,
-        [target.name]: target.value,
-      }));
+  const handleData = (
+    event:
+      | React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+      | React.MouseEvent<HTMLButtonElement>
+  ) => {
+    const target = event.target as HTMLInputElement | HTMLTextAreaElement;
+
+    setData((prev) => ({
+      ...prev,
+      [target.name]: target.value,
+    }));
+
+    setErrors((prev) => ({
+      ...prev,
+      [target.name]: null,
+    }));
+  };
+
+  const validate = () => {
+    const newErrors: FormErrors = {
+      hourlyPay: null,
+      startsAt: null,
+      workHour: null,
+    };
+
+    if (!data.hourlyPay) {
+      newErrors.hourlyPay = '시급을 입력해주세요.';
     }
+    if (!data.startsAt) {
+      newErrors.startsAt = '시작 일시를 입력해주세요.';
+    }
+    if (!data.workHour) {
+      newErrors.workHour = '시간을 입력해주세요.';
+    }
+
+    return newErrors;
   };
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    const validationErrors = validate();
+    if (Object.values(validationErrors).some((error) => error !== null)) {
+      setErrors(validationErrors);
+    } else {
+      // 유효성 검사를 통과한 경우, 데이터 제출
+    }
   };
 
   return (
-    <div className="">
+    <div className="flex-center tablet:w-680px pc:w-964px">
       <form onSubmit={handleSubmit}>
-        <FormGroup className="w-308px">
-          <FormGroup.Label htmlFor="hourlyPay">시급*</FormGroup.Label>
-          <FormGroup.InputWrapper className="flex input-base">
-            <FormGroup.InputField
-              id="hourlyPay"
-              name="hourlyPay"
-              type="number"
-              onChange={handleData}
-              placeholder="입력"
-            />
-            <span>원</span>
-          </FormGroup.InputWrapper>
-
-          <FormGroup.Label htmlFor="startsAt">시간*</FormGroup.Label>
-          <FormGroup.InputField.Text
-            id="startsAt"
-            name="startsAt"
-            className="h-48px"
-            onChange={handleData}
-            placeholder="입력"
-          />
-
-          <FormGroup.Label htmlFor="workHour">업무 시간*</FormGroup.Label>
-          <FormGroup.InputWrapper className="flex input-base">
-            <FormGroup.InputField
-              id="workHour"
-              name="workhour"
-              onChange={handleData}
-              placeholder="입력"
-            />
-            <span className="w-40px">시간</span>
-          </FormGroup.InputWrapper>
-
+        <FormGroup>
+          <div className="tablet:flex tablet:flex-wrap tablet:gap-20px pc:flex pc:gap-20px">
+            <div className="pc:flex gap-20px">
+              <div className="mb-20px">
+                <FormGroup.Label htmlFor="hourlyPay">시급*</FormGroup.Label>
+                <FormGroup.InputWrapper className="flex input-base">
+                  <FormGroup.InputField
+                    id="hourlyPay"
+                    name="hourlyPay"
+                    type="number"
+                    onChange={handleData}
+                  />
+                  <span>원</span>
+                </FormGroup.InputWrapper>
+                <FormGroup.ErrorMessage errorMessage={errors.hourlyPay} />
+              </div>
+              <div className="mb-40px h-58px">
+                <FormGroup.Label htmlFor="startsAt">시작 일시*</FormGroup.Label>
+                <FormGroup.InputField
+                  id="startsAt"
+                  name="startsAt"
+                  type="datetime-local"
+                  onChange={handleData}
+                  className="input-base"
+                />
+                <FormGroup.ErrorMessage errorMessage={errors.startsAt} />
+              </div>
+            </div>
+            <div className="mb-20px">
+              <FormGroup.Label htmlFor="workHour">업무 시간*</FormGroup.Label>
+              <FormGroup.InputWrapper className="flex input-base">
+                <FormGroup.InputField
+                  id="workHour"
+                  name="workHour"
+                  onChange={handleData}
+                />
+                <span className="w-40px">시간</span>
+              </FormGroup.InputWrapper>
+              <FormGroup.ErrorMessage errorMessage={errors.workHour} />
+            </div>
+          </div>
           <FormGroup.Label htmlFor="description">공고 설명</FormGroup.Label>
           <FormGroup.InputField.Textarea
             id="description"
             name="description"
             onChange={handleData}
-            placeholder="공고 설명을 입력해주세요"
+            placeholder="공고 설명을 입력하세요"
+            className="h-153px"
           />
         </FormGroup>
 
-        <Button type="submit" className="w-full button_medium_active">
-          등록하기
-        </Button>
+        <div className="mt-24px text-center">
+          <Button type="submit" className="button_large_active">
+            등록하기
+          </Button>
+        </div>
       </form>
     </div>
   );
