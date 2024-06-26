9837d43edba4c40bf7d161eb0c9b7a2bd98a7f27
eprnf <eprnf21@gmail.com>
2024-06-26 18:12:50 +0900

    DD-44-refactor: 스타일 적용방식 수정 및 href 올바르게 설정

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -9,28 +9,10 @@ import {
   IconLocationActive,
 } from '@/utils/Icons';
 
-export const POST_STATUS_STYLE_MAP = {
-  open: {
-    titleTextColor: 'text-black',
-    textColor: 'text-gray50',
-    alertTextColor: 'text-red40',
-    borderColor: 'bg-red40',
-    imageText: null,
-  },
-  closed: {
-    titleTextColor: 'text-black',
-    textColor: 'text-gray30',
-    alertTextColor: 'text-gray30',
-    borderColor: 'bg-gray30',
-    imageText: '마감 완료',
-  },
-  passed: {
-    titleTextColor: 'text-black',
-    textColor: 'text-gray50',
-    alertTextColor: 'text-red40',
-    borderColor: 'bg-red40',
-    imageText: '지난 공고',
-  },
+export const POST_IMAGE_STATUS_TEXT = {
+  open: null,
+  closed: '마감 완료',
+  passed: '지난 공고',
 } as const;
 
 interface PostProps {
@@ -69,7 +51,7 @@ function PostImage({
   imageUrl: string;
   status?: NoticeStatus;
 }) {
-  const statusText = POST_STATUS_STYLE_MAP[status].imageText;
+  const statusText = POST_IMAGE_STATUS_TEXT[status];
   const ImageOverlay = (
     <div className="absolute inset-0 flex-center text-16px tablet:text-28px pc:text-28px font-bold text-gray30 bg-black opacity-20">
       {statusText}
@@ -84,20 +66,10 @@ function PostImage({
   );
 }
 
-function Title({
-  className = '',
-  text,
-  status = 'open',
-}: {
-  className?: string;
-  text: string;
-  status?: NoticeStatus;
-}) {
-  const textColor = POST_STATUS_STYLE_MAP[status].titleTextColor;
-
+function Title({ className = '', text }: { className?: string; text: string }) {
   return (
     <h2
-      className={`text-18px tablet:text-24px pc:text-24px font-[700] tablet:mr-9px pc:mr-9px ${textColor} ${className}`}
+      className={`text-black marker:text-18px tablet:text-24px pc:text-24px font-[700] tablet:mr-9px pc:mr-9px ${className}`}
     >
       {text}
     </h2>
@@ -107,17 +79,13 @@ function Title({
 function SubTitle({
   className = '',
   text,
-  status = 'open',
 }: {
   className?: string;
   text: string;
-  status?: NoticeStatus;
 }) {
-  const textColor = POST_STATUS_STYLE_MAP[status].titleTextColor;
-
   return (
     <h4
-      className={`text-16px tablet:text-20px pc:text-20px font-[700] ${textColor} ${className}`}
+      className={`text-black text-16px tablet:text-20px pc:text-20px font-[700] ${className}`}
     >
       {text}
     </h4>
@@ -157,7 +125,6 @@ function WorkSchedule({
     formattedEndTime,
     durationHours,
   } = FormatUtils.workSchedule(startsAt, workHour);
-  const textColor = POST_STATUS_STYLE_MAP[status].textColor;
 
   const clockIcon =
     status === 'closed' ? (
@@ -167,9 +134,9 @@ function WorkSchedule({
     );
 
   return (
-    <div className={`flex gap-6px ${textColor} ${className}`}>
+    <div className={`flex gap-6px ${className}`}>
       {clockIcon}
-      <div className="flex gap-4px flex-wrap leading-[22px] text-12px tablet:text-14px pc:text-14px font-[400] break-keep">
+      <div className="flex gap-4px flex-wrap leading-[22px] text-gray50 text-12px tablet:text-14px pc:text-14px font-[400] break-keep">
         <p>{formattedStartDate}</p>
         <p>
           {formattedStartTime} ~ {formattedEndTime} ({durationHours}시간)
@@ -188,8 +155,6 @@ function Location({
   address: string;
   status?: NoticeStatus;
 }) {
-  const textColor = POST_STATUS_STYLE_MAP[status].textColor;
-
   const locationIcon =
     status === 'closed' ? (
       <IconLocationActive aria-label="장소 비활성화" />
@@ -201,7 +166,7 @@ function Location({
     <div className={`flex items-center gap-6px ${className}`}>
       {locationIcon}
       <p
-        className={`text-12px tablet:text-14px pc:text-14px font-[400] leading-[22px] ${textColor}`}
+        className={`text-gray50 text-12px tablet:text-14px pc:text-14px font-[400] leading-[22px]`}
       >
         {address}
       </p>
@@ -224,14 +189,18 @@ function HourlyPayPercentBadge({
   return <RateBadge rate={roundedPercentage} />;
 }
 
-const Post = Object.assign(PostWrapper, {
-  Image: PostImage,
-  Title: Title,
-  TitleBadge: TitleBadge,
-  SubTitle: SubTitle,
-  WorkSchedule: WorkSchedule,
-  Location: Location,
-  IncreaseRateBadge: HourlyPayPercentBadge,
-});
+const Post = Object.assign(
+  {},
+  {
+    Wrapper: PostWrapper,
+    Image: PostImage,
+    Title: Title,
+    TitleBadge: TitleBadge,
+    SubTitle: SubTitle,
+    WorkSchedule: WorkSchedule,
+    Location: Location,
+    IncreaseRateBadge: HourlyPayPercentBadge,
+  }
+);
 
 export default Post;
--- src/components/Post/PostCard.tsx
+++ src/components/Post/PostCard.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Post from '@/components/Post/Post';
+import FormatUtils from '@/lib/utils/FormatUtils';
 
 interface PostCardProps {
   item: Notice;
@@ -7,19 +8,26 @@ interface PostCardProps {
 }
 
 export default function PostCard({ item, className }: PostCardProps) {
-  const { hourlyPay, startsAt, closed, workhour, shop } = item;
+  const { id, hourlyPay, startsAt, closed, workhour, shop } = item;
   const { name, address1, imageUrl, originalHourlyPay } = shop.item;
+
   const isPassed = new Date() > new Date(startsAt);
   const currentPostState: NoticeStatus = closed
     ? 'closed'
     : isPassed
       ? 'passed'
       : 'open';
-  const formattedPay = hourlyPay.toLocaleString() + '원';
+
+  const formattedPay = FormatUtils.price(hourlyPay) + '원';
+  const { formattedUrl } = FormatUtils.parseIDAndFormatUrl(shop.href);
+  const noticeHref = formattedUrl + `/notices/${id}`;
+
+  const statusStyle =
+    currentPostState !== 'open' ? '[&_*]:mix-blend-luminosity' : '';
 
   return (
-    <Link href={shop.href} className={`${className}`}>
-      <Post
+    <Link href={noticeHref} className={`${statusStyle} ${className}`}>
+      <Post.Wrapper
         status={currentPostState}
         className={`flex flex-col justify-between w-full min-h-[16.3rem] tablet:min-h-[21.8rem] pc:min-h-[21.8rem] bg-white rounded-12px p-12px tablet:p-16px pc:p-16px border border-gray20 ${closed ? 'pointer-events-none' : ''}`}
       >
@@ -29,7 +37,7 @@ export default function PostCard({ item, className }: PostCardProps) {
           className="w-full aspect-video mb-12px tablet:mb-20px"
         />
         <div className="flex flex-col gap-8px">
-          <Post.SubTitle text={name} status={currentPostState} />
+          <Post.SubTitle text={name} />
           <Post.WorkSchedule
             startsAt={startsAt}
             workHour={workhour}
@@ -38,17 +46,13 @@ export default function PostCard({ item, className }: PostCardProps) {
           <Post.Location address={address1} status={currentPostState} />
         </div>
         <div className="flex justify-between flex-col pt-16px gap-0px tablet:flex-row pc:flex-row mt-auto">
-          <Post.Title
-            text={formattedPay}
-            status={currentPostState}
-            className="shrink-0"
-          />
+          <Post.Title text={formattedPay} className="shrink-0" />
           <Post.IncreaseRateBadge
             hourlyPay={hourlyPay}
             originalHourlyPay={originalHourlyPay}
           />
         </div>
-      </Post>
+      </Post.Wrapper>
     </Link>
   );
 }
