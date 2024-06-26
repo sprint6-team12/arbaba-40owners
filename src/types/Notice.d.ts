480da5c4caf9be1f135ba84e2edd0e3b375c9042
eprnf <eprnf21@gmail.com>
2024-06-26 22:23:46 +0900

    fix: 데이터 타입에 따른 오류 수정

--- src/types/Notice.d.ts
+++ src/types/Notice.d.ts
@@ -1,16 +1,23 @@
-interface Notice {
+interface NoticeBase {
   id: string;
   hourlyPay: number;
   startsAt: string;
   workhour: number;
   description: string;
   closed: boolean;
+}
+
+interface Notice extends NoticeBase {}
+
+interface currentUserApplication {
+  id: string; // application.id
+  status: UserApplicationStatus; // application.status
+  createdAt: string; // application.createdAt
+}
+
+interface NoticeEmployee extends NoticeBase {
   shop: ShopData;
   currentUserApplication?: { item: currentUserApplication };
 }
 
-interface currentUserApplication {
-  id: string; // application.id,
-  status: UserApplicationStatus; // application.status
-  createdAt: string; // application.createdAt
-}
+interface NoticeEmployer extends NoticeBase {}
