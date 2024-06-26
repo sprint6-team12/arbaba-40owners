3ffbdf926e154091df1b8a7b49c459e9dbe7d305
chanhee won <wch2208@gmail.com>
2024-06-26 12:01:02 +0900

    DD-46-refactor: 불필요한 코드 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,3 @@
-import RateBadge from '@/components/Badge/RateBadge';
-
 export default function Home() {
-  return (
-    <main>
-      <RateBadge rate={100} />
-    </main>
-  );
+  return <main></main>;
 }
