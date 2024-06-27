d0809398a0b0587f2ce688a3d913970829dd8906
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-06-27 13:40:34 +0900

    DD-31-refactor: page/ShopDetail SSR 적용

--- src/pages/shops/[shop_id]/index.tsx
+++ src/pages/shops/[shop_id]/index.tsx
@@ -1,5 +1,4 @@
 import { GetServerSideProps } from 'next';
-import { useState } from 'react';
 import PostCard from '@/components/Post/PostCard';
 import ShopNoData from '@/components/ShopDetail/ShopNoData';
 import ShopTitle from '@/components/ShopDetail/ShopTitle/ShopTitle';
@@ -8,7 +7,7 @@ import noticeAPI from '@/utils/api/noticeAPI';
 import shopAPI from '@/utils/api/shopAPI';
 
 interface ShopDetailProps {
-  shopDatas: Shops | null;
+  shopData: Shops | null;
   noticesDatas: Notices | null;
 }
 
@@ -18,18 +17,18 @@ export const getServerSideProps: GetServerSideProps<ShopDetailProps> = async (
   const { shop_id } = context.query;
   const shopId = shop_id as string;
 
-  let shopDatas: Shops | null = null;
+  let shopData: Shops | null = null;
   let noticesDatas: Notices | null = null;
 
   try {
     if (shopId) {
       const shopResponse = await shopAPI.getShop(shopId);
-      shopDatas = shopResponse.item ?? null;
+      shopData = shopResponse.item ?? null;
 
       const noticesResponse = await noticeAPI.getShopNoticeList(shopId, {
         shop_id: shopId,
         offset: 0,
-        limit: 6,
+        limit: 99,
       });
       noticesDatas = noticesResponse ?? null;
     }
@@ -39,74 +38,122 @@ export const getServerSideProps: GetServerSideProps<ShopDetailProps> = async (
 
   return {
     props: {
-      shopDatas,
+      shopData,
       noticesDatas,
     },
   };
 };
 
 export default function ShopDetail({
-  shopDatas,
+  shopData,
   noticesDatas,
 }: ShopDetailProps) {
-  const [shopData] = useState<Shops | null>(shopDatas);
-  const [noticesData] = useState<Notices | null>(noticesDatas);
-  const [isLoading] = useState<boolean>(false);
+  // const [noticesData, setNoticesData] = useState<Notices | null>(noticesDatas);
+  // const [offset, setOffset] = useState<number>(3);
   // const router = useRouter();
+
+  // const observer = useRef<IntersectionObserver | null>(null);
+  // const observerRef = useRef<HTMLDivElement | null>(null);
 
   // const { shop_id } = router.query;
   // const shop_id = '7367ccac-bcce-4203-9bb9-65098fffb05e';
 
   // const shopId = shop_id as string;
 
+  // const fetchMoreNotices = async () => {
+  //   if (!noticesData?.hasNext) return;
+
+  //   console.log(`before: ${noticesData}`);
+
+  //   try {
+  //     const newNoticesData = await noticeAPI.getShopNoticeList(shopId, {
+  //       shop_id: shopId,
+  //       offset: offset,
+  //       limit: 3,
+  //     });
+  //     setNoticesData((prevData) => ({
+  //       ...newNoticesData,
+  //       items: [...(prevData?.items || []), ...newNoticesData.items],
+  //     }));
+  //     setOffset((prev) => prev + 3);
+  //   } catch (error) {
+  //     // error
+  //   }
+
+  //   console.log(`after: ${noticesData}`);
+  // };
+
+  // useEffect(() => {
+  //   if (!noticesData?.hasNext) return;
+  //   if (observer.current) observer.current.disconnect();
+
+  //   if (typeof IntersectionObserver === 'undefined') {
+  //     // IntersectionObserver가 지원되지 않는 경우
+  //     return;
+  //   }
+
+  //   const handleObserver = (entries: IntersectionObserverEntry[]) => {
+  //     const target = entries[0];
+  //     if (target.isIntersecting) {
+  //       fetchMoreNotices();
+  //     }
+  //   };
+
+  //   observer.current = new IntersectionObserver(handleObserver);
+
+  //   if (observerRef.current) {
+  //     observer.current.observe(observerRef.current);
+  //   }
+
+  //   return () => {
+  //     if (observer.current) {
+  //       observer.current.disconnect();
+  //     }
+  //   };
+  // }, [noticesData?.hasNext]);
+
   return (
     <>
-      {isLoading ? (
-        <div>loading...</div>
-      ) : (
-        <div className="w-full flex flex-col">
-          <div className="mx-auto w-full my-20px px-32px py-60px max-w-[1000px]">
-            <h1 className="font-bold text-start text-28px mb-24px">내 가게</h1>
-            {!shopData ? (
-              <ShopNoData
-                title="내 가게를 소개하고 공고도 등록해 보세요."
-                text="내 가게 등록하기"
-              />
-            ) : (
-              <div className="flex flex-col items-start justify-start gap-0 p-24px rounded-24px bg-red10 min-h-[358px] pc:flex-row pc:gap-31px">
-                <ShopTitle shopData={shopData} />
-              </div>
-            )}
-          </div>
-          {shopData && (
-            <div className="bg-[#fafafa]">
-              <div className="mx-auto w-full px-32px py-60px max-w-[1000px]">
-                <h1 className="font-bold text-start text-28px mb-24px pt-40px">
-                  등록한 공고
-                </h1>
-                {!noticesData ? (
-                  <ShopNoData
-                    title="공고를 등록해보세요."
-                    text="공고 등록하기"
-                  />
-                ) : (
-                  <div className="flex gap-32px">
-                    <div className="grid grid-cols-2 gap-x-8px gap-y-12px my-0 mx-auto pc:gap-x-36px pc:gap-y-26px pc:grid-cols-3">
-                      {noticesData.items.map((item) => (
-                        <PostCard
-                          key={item.item.id}
-                          noticeData={item.item}
-                          shopData={shopData}
-                        />
-                      ))}
-                    </div>
-                  </div>
-                )}
-              </div>
+      <div className="w-full flex flex-col">
+        <div className="mx-auto w-full my-20px px-32px py-60px max-w-[1000px]">
+          <h1 className="font-bold text-start text-28px mb-24px">내 가게</h1>
+          {!shopData ? (
+            <ShopNoData
+              title="내 가게를 소개하고 공고도 등록해 보세요."
+              text="내 가게 등록하기"
+            />
+          ) : (
+            <div className="flex flex-col items-start justify-start gap-0 p-24px rounded-24px bg-red10 min-h-[358px] pc:flex-row pc:gap-31px">
+              <ShopTitle shopData={shopData} />
             </div>
           )}
         </div>
-      )}
+        {shopData && (
+          <div className="bg-[#fafafa]">
+            <div className="mx-auto w-full px-32px py-60px max-w-[1000px]">
+              <h1 className="font-bold text-start text-28px mb-24px pt-40px">
+                등록한 공고
+              </h1>
+              {!noticesDatas ? (
+                <ShopNoData title="공고를 등록해보세요." text="공고 등록하기" />
+              ) : (
+                <div className="flex gap-32px">
+                  <div className="grid grid-cols-2 gap-x-8px gap-y-12px my-0 mx-auto pc:gap-x-36px pc:gap-y-26px pc:grid-cols-3">
+                    {noticesDatas.items.map((item) => (
+                      <PostCard
+                        key={item.item.id}
+                        noticeData={item.item}
+                        shopData={shopData}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+      {/* {noticesData?.hasNext && <div ref={observerRef}></div>} */}
     </>
   );
 }
