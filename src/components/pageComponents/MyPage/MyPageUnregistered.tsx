3847142edaabe37358c46c9d026ba8d51a96ba58
doh-yo <doyo159090@gmail.com>
2024-06-28 19:18:18 +0900

    DD-32-refactor: shopnodata컴포넌트적용

--- src/components/pageComponents/MyPage/MyPageUnregistered.tsx
+++ src/components/pageComponents/MyPage/MyPageUnregistered.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Button from '@/components/Button/Button';
+import ShopNoData from '@/components/ShopDetail/ShopNoData';
 import { IconCloseBlack } from '@/utils/Icons';
 import MyPageInput from './MyPageInput';
 
@@ -12,7 +12,7 @@ export default function MyPageUnregistered({
 
   return (
     <div className="px-40px tablet:px-60px pc:px-[238px] py-12px tablet:py-32px pc:py-60px">
-      <div className="flex justify-between">
+      <div className="flex justify-between mb-16px">
         <span className="font-[700] text-20px tablet:text-28px pc:text-28px">
           내 프로필
         </span>
@@ -23,21 +23,10 @@ export default function MyPageUnregistered({
         )}
       </div>
       {!userProfileData && !showInputForm ? (
-        <div className="border rounded-12px w-full h-195px tablet:h-217px pc:h-217px mt-16px">
-          <div className="text-center px-24px py-60px">
-            <span className="font-[400] text-14px tablet:text-16px pc:text-16px">
-              내 프로필을 등록하고 원하는 가게에 지원해 보세요.
-            </span>
-            <div className="flex-center mt-16px tablet:mt-24px pc:mt-24px">
-              <Button
-                onClick={() => setShowInputForm(true)}
-                className="button_large_active text-nowrap"
-              >
-                내 프로필 등록 하기
-              </Button>
-            </div>
-          </div>
-        </div>
+        <ShopNoData
+          title="내 프로필을 등록하고 원하는 가게에 지원해 보세요."
+          text="내 프로필 등록하기"
+        /> //내프로필등록하기버튼누르면입력폼이떠야됨
       ) : (
         <div>
           <MyPageInput />
