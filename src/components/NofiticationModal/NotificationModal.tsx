f251ae35829a6a0f2930064f7d51e3d46607afac
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-06-29 16:34:58 +0900

    style: NotificationMoadl 모바일 반응형 수정

--- src/components/NofiticationModal/NotificationModal.tsx
+++ src/components/NofiticationModal/NotificationModal.tsx
@@ -2,29 +2,34 @@ import { NotificationModalProps } from '@/types/NotificationModal';
 import { IconCloseBlack } from '@/utils/Icons';
 import AlertItem from './AlertItem';
 
-export default function NotificationModal({ data, onClose }: NotificationModalProps) {
+export default function NotificationModal({
+  data,
+  onClose,
+}: NotificationModalProps) {
   const items = data?.items || [];
   const filteredItem = items.filter(({ item }) => {
     const oneWeekAgo = new Date();
-    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 20);
     const itemDate = new Date(item.createdAt);
 
     return !item.read || (item.read && itemDate > oneWeekAgo);
   });
 
   return (
-    <div className="w-350px h-400px rounded-10px bg-red10 border-1px py-24px px-20px">
-      <div className="flex flex-col w-full h-full gap-12px">
-        <div className="flex items-center justify-between">
-          <p className="font-bold text-20px">알림 {filteredItem.length}개</p>
-          <IconCloseBlack onClick={onClose} className="cursor-pointer"/>
-        </div>
-        <div
-          className={`flex flex-col gap-12px ${filteredItem.length > 2 && 'overflow-y-scroll'}`}
-        >
-          {filteredItem.map(({ item }) => (
-            <AlertItem key={item.id} item={item} />
-          ))}
+    <div className='fixed tablet:relative pc:relative inset-0 flex z-50'>
+      <div className="relative w-full h-full tablet:right-0 pc:right-0 tablet:top-12px pc:top-12px tablet:absolute pc:absolute tablet:w-350px tablet:h-400px pc:w-350px pc:h-400px bg-red10 border border-1px rounded-10px overflow-y-auto">
+        <div className="flex flex-col w-full h-full gap-12px py-24px px-20px">
+          <div className="flex items-center justify-between">
+            <p className="font-bold text-20px">알림 {filteredItem.length}개</p>
+            <IconCloseBlack onClick={onClose} className="cursor-pointer" />
+          </div>
+          <div
+            className={`flex flex-col gap-12px ${filteredItem.length > 2 && 'overflow-y-scroll'}`}
+          >
+            {filteredItem.map(({ item }) => (
+              <AlertItem key={item.id} item={item} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
