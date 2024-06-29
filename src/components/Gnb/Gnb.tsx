9b8fbeb520a59ca410391ddbf160a2b81c636334
doyo <83871696+doh-yo@users.noreply.github.com>
2024-06-29 21:54:22 +0900

    ♻️ refactor: gnb API연결 및 버튼 로직 수정 (#39)
    * DD-50-refactor: 버튼 수정 및 API
    
    * DD-50-style: footer 스타일 수정
    
    * DD-50-refactor: putalerts 제거

--- src/components/Gnb/Gnb.tsx
+++ src/components/Gnb/Gnb.tsx
@@ -1,45 +1,46 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from '/public/images/logo.png';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { userState } from '@/recoil/atoms/AuthAtom';
 import GnbButton from './GnbButton';
+import NotificationButton from './NotificationButton';
 import SearchBar from './SearchBar';
 
-interface GnbProps {
-  onClick: (pathname?: string) => void;
-  hasNotification?: boolean;
-}
-
-export default function Gnb({ onClick, hasNotification }: GnbProps) {
+export default function Gnb() {
   const { type } = useRecoilValue(userState);
   const [isHydrated, setIsHydrated] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     setIsHydrated(true);
   }, []);
+
+  const handleGnbButtonsClick = (pathname: string | undefined) => {
+    if (pathname) {
+      router.push(pathname);
+    }
+  };
 
   if (!isHydrated) {
     return null;
   }
 
   return (
-    <div className="flex justify-between items-center flex-wrap px-20px tablet:px-32px pc:px-[208px] py-10px tablet:py-15px pc:py-15px">
+    <div className="flex justify-between items-center flex-wrap px-5 tablet:px-8 pc:px-52 py-2.5 tablet:py-4 pc:py-4">
       <Link href="/">
-        <div className="flex tablet:flex-center pc:flex-center w-81px tablet:w-112px pc:w-112px h-15px tablet:h-40px pc:h-40px">
+        <div className="flex items-center w-20 tablet:w-28 pc:w-28 h-4 tablet:h-10 pc:h-10">
           <Image src={Logo} alt="더줄게로고" />
         </div>
       </Link>
-      <div className="flex-center order-1 tablet:order-none pc:order-none">
+      <div className="flex items-center order-2 tablet:order-none pc:order-none mt-16px tablet:mt-0 pc:mt-0">
         <SearchBar />
       </div>
-      <div>
-        <GnbButton
-          userType={type}
-          onClick={onClick}
-          hasNotification={hasNotification}
-        />
+      <div className="flex items-center justify-between gap-16px tablet:gap-40px pc:gap-40px">
+        <GnbButton userType={type} onClick={handleGnbButtonsClick} />
+        {type !== 'guest' && <NotificationButton />}
       </div>
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
 import { RecoilRoot } from 'recoil';
 import Footer from '@/components/Footer/footer';
 import Gnb from '@/components/Gnb/Gnb';
@@ -8,18 +7,11 @@ import PopupsWrapper from '@/components/Popup/PopupsWrapper';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const handleGnbButtonsClick = (pathname: string | undefined) => {
-    if (pathname) {
-      router.push(pathname);
-    }
-  };
-
   return (
     <RecoilRoot>
       <ModalsWrapper />
       <PopupsWrapper />
-      <Gnb onClick={handleGnbButtonsClick} />
+      <Gnb />
       <Component {...pageProps} />
       <Footer />
     </RecoilRoot>
