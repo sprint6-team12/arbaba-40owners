f293092d66949f6ad9b28be3adea918f1f782e1b
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-06-29 16:57:12 +0900

    feat: NotificationMoadl 알림 읽음 기능 추가 및 Link 연결

--- src/components/NofiticationModal/AlertItem.tsx
+++ src/components/NofiticationModal/AlertItem.tsx
@@ -1,30 +1,55 @@
+import Link from 'next/link';
+import { useRecoilValue } from 'recoil';
 import FormatUtils from '@/lib/utils/FormatUtils';
 import { getResultInfo } from '@/lib/utils/NotificationModal';
+import { userState } from '@/recoil/atoms/AuthAtom';
 import { AlertItemProps } from '@/types/NotificationModal';
+import alertAPI from '@/utils/api/alertAPI';
 
 export default function AlertItem({ item }: { item: AlertItemProps }) {
   const { notice, result, shop, read } = item;
+  const href = item.notice.href;
   const resultInfo = getResultInfo(result);
+  const { id } = useRecoilValue(userState);
 
   if (!resultInfo) {
     return null;
   }
 
+  function removePrefix(url: string): string {
+    const prefix = '/api/6-12/the-julge';
+    if (url.startsWith(prefix)) {
+      return url.slice(prefix.length);
+    }
+    return url;
+  }
+
+  const linkHref = removePrefix(href);
+
   const { formattedStartDate, formattedStartTime, formattedEndTime } =
     FormatUtils.workSchedule(notice.item.startsAt, notice.item.workhour);
   const formattedDate = FormatUtils.distanceToNow(item.createdAt);
 
+  const handleOnclickAlert = async (alertId: string) => {
+    if (id && !read) {
+      await alertAPI.putAlerts({ user_id: id, alert_id: alertId });
+    }
+  };
+
   return (
-    <div
-      key={item.id}
-      className={`flex flex-col p-4 border rounded gap-8px ${read ? 'bg-gray20' : 'bg-white'} `}
-    >
-      {resultInfo.icon}
-      <p className={`text-14px ${read ? 'text-gray50' : ''}`}>
-        {shop.item.name} ({formattedStartDate} {formattedStartTime} ~{' '}
-        {formattedEndTime}) 공고 지원이 {resultInfo.text}되었어요
-      </p>
-      <p className="text-12px text-gray40">{formattedDate}</p>
-    </div>
+    <Link href={linkHref}>
+      <div
+        onClick={() => handleOnclickAlert(item.id)}
+        key={item.id}
+        className={`flex flex-col p-4 border rounded gap-8px ${read ? 'bg-gray20' : 'bg-white'} `}
+      >
+        {resultInfo.icon}
+        <p className={`text-14px ${read ? 'text-gray50' : ''}`}>
+          {shop.item.name} ({formattedStartDate} {formattedStartTime} ~{' '}
+          {formattedEndTime}) 공고 지원이 {resultInfo.text}되었어요
+        </p>
+        <p className="text-12px text-gray40">{formattedDate}</p>
+      </div>
+    </Link>
   );
 }
--- src/components/NofiticationModal/NotificationModal.tsx
+++ src/components/NofiticationModal/NotificationModal.tsx
@@ -17,7 +17,7 @@ export default function NotificationModal({
 
   return (
     <div className='fixed tablet:relative pc:relative inset-0 flex z-50'>
-      <div className="relative w-full h-full tablet:right-0 pc:right-0 tablet:top-12px pc:top-12px tablet:absolute pc:absolute tablet:w-350px tablet:h-400px pc:w-350px pc:h-400px bg-red10 border border-1px rounded-10px overflow-y-auto">
+      <div className="relative w-full h-full tablet:right-0 pc:right-0 tablet:top-12px pc:top-12px tablet:absolute pc:absolute tablet:w-350px tablet:h-400px pc:w-350px pc:h-400px bg-red10 border rounded-10px overflow-y-auto">
         <div className="flex flex-col w-full h-full gap-12px py-24px px-20px">
           <div className="flex items-center justify-between">
             <p className="font-bold text-20px">알림 {filteredItem.length}개</p>
