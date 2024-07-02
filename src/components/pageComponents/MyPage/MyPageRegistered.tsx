e59ee126034e8939a42047f488178edc61829972
doh-yo <doyo159090@gmail.com>
2024-07-02 21:38:12 +0900

    DD-32-style: 스타일 수정

--- src/components/pageComponents/MyPage/MyPageRegistered.tsx
+++ src/components/pageComponents/MyPage/MyPageRegistered.tsx
@@ -15,7 +15,6 @@ export default function MyPageRegistered({
   profileData,
 }: MyPageRegisteredProps) {
   const [isEditing, setIsEditing] = useState(false);
-  // const [hasApplications, setHasApplications] = useState(false);
   const router = useRouter();
 
   const handleEditClick = () => {
@@ -26,7 +25,7 @@ export default function MyPageRegistered({
   };
 
   return (
-    <div className="px-[10px] tablet:px-[16px] pc:px-[60px] py-[3px] tablet:py-[8px] pc:py-[15px]">
+    <div className="px-[10px] tablet:px-[16px] pc:px-[60px] py-[3px] tablet:py-[8px] pc:py-[15px] mb-80px">
       <div className="flex justify-between items-center gap-[2.5px]">
         <span className="font-bold text-[20px] tablet:text-[28px] pc:text-[28px]">
           내 프로필
@@ -39,17 +38,15 @@ export default function MyPageRegistered({
       </div>
       <>
         <MyPageProfile ProfileData={profileData} onClick={handleEditClick} />
-        <div className="mt-[8px] mb-[8px]">
+        <div className="mt-40px mb-16px">
           <span className="font-bold text-[20px] tablet:text-[28px] pc:text-[28px]">
             신청 내역
           </span>
         </div>
-        {/* {hasApplications ? ( */}
         <div className="mt-[8px] w-full">
-          {/* <EmployeeTable data={dummyEmployeeTableData} /> */}
+          {/* <EmployeeTable data={data} /> */}
         </div>
-        {/* ) : ( */}
-        <div className="border rounded-[3px] w-full h-[48px] tablet:h-[54px] pc:h-[54px] flex flex-col justify-center items-center">
+        <div className="border rounded-12px w-full h-195px tablet:h-217px pc:h-217px flex flex-col justify-center items-center">
           <div className="text-center px-[6px] py-[15px]">
             <span className="font-normal text-[14px] tablet:text-[16px] pc:text-[16px]">
               아직 신청 내역이 없어요.
@@ -61,7 +58,6 @@ export default function MyPageRegistered({
             </div>
           </div>
         </div>
-        {/* )} */}
       </>
     </div>
   );
