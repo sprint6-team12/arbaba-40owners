bc921c22918376584fb9f060d9f1839d8ab26866
wch2208 <wch2208@gmail.com>
2024-07-04 22:34:01 +0900

    feat: 검색 결과 없는 경우 표시할 내용 추가

--- src/components/pageComponents/SearchPage/SearchPage.tsx
+++ src/components/pageComponents/SearchPage/SearchPage.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { FaShopSlash } from 'react-icons/fa6';
 import { RiLoader2Fill } from 'react-icons/ri';
 import { useRecoilValue } from 'recoil';
 import NoticeListView from '@/components/pageComponents/NoticeList/NoticeListView';
@@ -9,10 +10,14 @@ function SearchPage() {
   const searchValue = useRecoilValue(keywordDataState);
   const searchResults = useRecoilValue(searchResultState);
 
-  const isSearchComplete = useMemo(() => {
+  const hasResults = useMemo(() => {
     return (
       searchResults && searchResults.items && searchResults.items.length > 0
     );
+  }, [searchResults]);
+
+  const isSearchComplete = useMemo(() => {
+    return searchResults !== null;
   }, [searchResults]);
 
   if (!isSearchComplete) {
@@ -32,6 +37,12 @@ function SearchPage() {
           </>
         }
       />
+      {!hasResults && (
+        <div className="mx-auto w-fit h-340px text-20px tablet:text-28px pc:text-30px">
+          <FaShopSlash className="mx-auto mb-20px w-160px h-160px tablet:w-200px tablet:h-200px pc:w-240px pc:h-240px" />
+          공고 목록이 없습니다.
+        </div>
+      )}
     </>
   );
 }
