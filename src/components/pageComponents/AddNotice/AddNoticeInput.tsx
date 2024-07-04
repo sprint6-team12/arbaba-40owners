2a788fea1bd6023068261fd29df06262b8cc0479
doh-yo <doyo159090@gmail.com>
2024-07-04 14:58:41 +0900

    DD-92-refactor: Error 모달 추가

--- src/components/pageComponents/AddNotice/AddNoticeInput.tsx
+++ src/components/pageComponents/AddNotice/AddNoticeInput.tsx
@@ -87,10 +87,12 @@ export default function AddNoticeInput() {
 
           router.push('/');
         } else {
-          throw new Error('유효하지 않은 ID');
+          handleOpenConfirmModal('유효하지 않은 ID입니다.');
         }
       } catch (error) {
-        alert(error);
+        handleOpenConfirmModal(
+          '등록 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.'
+        );
       }
     }
   };
