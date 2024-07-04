eba1b01058bd74d18c42adbd85a4153cd4ff962b
wch2208 <wch2208@gmail.com>
2024-07-05 00:36:57 +0900

    refactor: 검색 결과 없음 표시

--- src/components/pageComponents/SearchPage/SearchPage.tsx
+++ src/components/pageComponents/SearchPage/SearchPage.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from 'next/router';
 import { useMemo } from 'react';
 import { FaShopSlash } from 'react-icons/fa6';
 import { RiLoader2Fill } from 'react-icons/ri';
@@ -8,48 +7,47 @@ import keywordDataState from '@/recoil/atoms/searchAtom';
 import searchResultState from '@/recoil/atoms/SearchResultAtom';
 
 function SearchPage() {
-  const router = useRouter();
   const searchValue = useRecoilValue(keywordDataState);
   const searchResults = useRecoilValue(searchResultState);
 
+  const isLoading = useMemo(() => {
+    return searchResults === null;
+  }, [searchResults]);
+
   const hasResults = useMemo(() => {
-    return (
-      searchResults && searchResults.items && searchResults.items.length > 0
-    );
-  }, [searchResults]);
+    return !isLoading && searchResults.items && searchResults.items.length > 0;
+  }, [isLoading, searchResults]);
 
-  const isSearchComplete = useMemo(() => {
-    return searchResults !== null;
-  }, [searchResults]);
+  const isEmptySearchResult = useMemo(() => {
+    return !isLoading && !hasResults;
+  }, [isLoading, hasResults]);
 
-  const isFiltered = useMemo(() => {
-    return !!router.query.startsAtGte;
-  }, [router.query.startsAtGte]);
-
-  if (!isSearchComplete) {
+  if (isLoading) {
     return (
       <RiLoader2Fill className="animate-spin w-28px h-28px mx-auto my-40px" />
     );
   }
 
+  if (isEmptySearchResult) {
+    return (
+      <div className="mx-auto w-fit h-340px text-20px tablet:text-28px pc:text-34px">
+        <FaShopSlash className="mx-auto mb-20px w-160px h-160px tablet:w-200px tablet:h-200px pc:w-260px pc:h-260px" />
+        <span className="text-red40">{searchValue}</span>에 해당하는 목록이
+        없습니다.
+      </div>
+    );
+  }
+
   return (
-    <>
-      <NoticeListView
-        key={searchValue}
-        initialData={searchResults}
-        title={
-          <>
-            <span className="text-red40">{searchValue}</span>에 대한 공고 목록
-          </>
-        }
-      />
-      {(isFiltered || !hasResults) && (
-        <div className="mx-auto w-fit h-340px text-20px tablet:text-28px pc:text-30px">
-          <FaShopSlash className="mx-auto mb-20px w-160px h-160px tablet:w-200px tablet:h-200px pc:w-240px pc:h-240px" />
-          공고 목록이 없습니다.
-        </div>
-      )}
-    </>
+    <NoticeListView
+      key={searchValue}
+      initialData={searchResults}
+      title={
+        <>
+          <span className="text-red40">{searchValue}</span>에 대한 공고 목록
+        </>
+      }
+    />
   );
 }
 
