27a08a427a8ccc730e1efe0145312674bfb0b218
wch2208 <wch2208@gmail.com>
2024-07-05 22:04:51 +0900

    refactor: EmployeeTable 내부 컴포넌트로 만들어 분리
    - 상수는 규칙에 맞게 네이밍 수정

--- src/components/Table/EmployeeTable.tsx
+++ src/components/Table/EmployeeTable.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
-import StatusBadge from '@/components/Badge/StatusBadge';
 import useMediaQuery from '@/hooks/useMediaQuery';
-import FormatUtils from '@/lib/utils/FormatUtils';
+import EmployeeTableBody from './EmployeeTableBody';
+import EmployeeTableHeader from './EmployeeTableHeader';
 
 export interface EmployeeTableApplication {
   id: string;
@@ -35,29 +35,6 @@ interface EmployeeTableProps {
 function EmployeeTable({ data }: EmployeeTableProps) {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
   const { isMobile, isTablet } = useMediaQuery();
-  const baseThStyle =
-    'border-gray20 border-1px bg-red10 text-left h-40px text-12px tablet:text-14px pc:text-14px';
-  const baseTdStyle =
-    'border-gray20 border-1px bg-white h-46px tablet:h-69px pc:h-69px text-14px tablet:text-16px pc:text-16px';
-
-  const tableHeaders = [
-    {
-      label: '가게',
-      className: `${baseThStyle} min-w-189px tablet:min-w-[228px] pc:w-228px sticky left-0 p-0 pl-8px`,
-    },
-    {
-      label: '일자',
-      className: `${baseThStyle} min-w-162px tablet:min-w-[300px] pc:w-300px pl-8px`,
-    },
-    {
-      label: '시급',
-      className: `${baseThStyle} min-w-162px pl-8px`,
-    },
-    {
-      label: '상태',
-      className: `${baseThStyle} min-w-162px tablet:min-w-[220px] pc:w-236px pl-12px`,
-    },
-  ];
 
   useEffect(() => {
     if (scrollContainerRef.current) {
@@ -68,54 +45,12 @@ function EmployeeTable({ data }: EmployeeTableProps) {
 
   return (
     <div
-      className="border-1px border-gray20 rounded-tr-10px rounded-tl-10px overflow-x-auto m-auto w-351px tablet:min-w-[680px] pc:min-w-[964px]"
+      className="rounded-tr-10px rounded-tl-10px overflow-x-auto m-auto w-351px tablet:min-w-[680px] pc:min-w-[964px]"
       ref={scrollContainerRef}
     >
       <table className="table-auto min-w-full">
-        <thead>
-          <tr>
-            {tableHeaders.map(({ className, label }, index) => (
-              <th key={index} className={className}>
-                {label}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.items.map(({ item }) => {
-            const { id, shop, notice, status } = item;
-            const { name } = shop.item;
-            const { hourlyPay, workhour, startsAt } = notice.item;
-            const formattedSchedule = FormatUtils.workSchedule(
-              startsAt,
-              workhour
-            ).formattedSchedule;
-            const formattedPay = FormatUtils.price(hourlyPay);
-
-            return (
-              <tr key={id}>
-                <td
-                  className={`${baseTdStyle} min-w-189px tablet:min-w-[228px] pc:w-228px sticky left-0px pl-8px`}
-                >
-                  {name}
-                </td>
-                <td
-                  className={`${baseTdStyle} min-w-162px tablet:min-w-[300px] pl-8px`}
-                >
-                  {formattedSchedule}
-                </td>
-                <td className={`${baseTdStyle} min-w-162px pl-8px`}>
-                  {formattedPay}원
-                </td>
-                <td
-                  className={`${baseTdStyle} min-w-162px tablet:min-w-[220px] pc:w-236px pl-12px`}
-                >
-                  <StatusBadge status={status} />
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
+        <EmployeeTableHeader />
+        <EmployeeTableBody items={data.items} />
       </table>
     </div>
   );
