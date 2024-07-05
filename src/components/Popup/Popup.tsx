d5d233a225349485ae53bb8dc7f19c947a9a0a73
Jeongmin <154623483+oris8@users.noreply.github.com>
2024-07-05 14:33:12 +0900

    DD-101-refactor: fadeOut방식 변경

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -22,14 +22,11 @@ const Popup = ({
     setIsVisible(false);
   };
 
-  // isVisible false일때 fadeDuration동안 기다렸다가 close
-  // setTimeout을 이용해서 상태를 관리하느라 fadeDuration에 -50 해주지 않으면 깜박거리는 이슈가 있음
-  useEffect(() => {
-    if (!isVisible) {
-      const timer = setTimeout(() => onClose(), fadeDuration - 50);
-      return () => clearTimeout(timer);
+  const handleAnimationEnd = (event: React.AnimationEvent<HTMLDivElement>) => {
+    if (event.animationName === 'fadeOut') {
+      onClose();
     }
-  }, [isVisible, onClose]);
+  };
 
   // duration 있으면 자동으로 Close
   useEffect(() => {
@@ -44,6 +41,7 @@ const Popup = ({
   return (
     <div
       className={`min-w-113px h-46px rounded-5px flex gap-8px justify-around items-center bg-red30 text-white text-16px px-16px ${fadeAnimationStyle}`}
+      onAnimationEnd={handleAnimationEnd}
     >
       <p className="block">{message}</p>
       {!duration && <button onClick={handleClose}>x</button>}
