177fb673c59bcbde2624920b85bff32a5f9feb56
wch2208 <wch2208@gmail.com>
2024-07-06 15:05:26 +0900

    refactor: 상수 대문자로 수정, Tailwind 문법에 맞게 글로벌 css 수정

--- src/components/Table/ActionButton.tsx
+++ src/components/Table/ActionButton.tsx
@@ -7,17 +7,17 @@ interface ActionButtonProps
 }
 
 function ActionButton({ href, ...rest }: ActionButtonProps) {
-  const styleBase =
+  const BASE_STYLE =
     'tablet:font-bold pc:font-bold border-1px flex-center w-69px tablet:w-92px pc:w-92px h-32px tablet:h-38px pc:h-38px text-12px tablet:text-14px pc:text-14px rounded-6px';
 
-  const applyResponseMap: { [key: string]: string } = {
+  const APPLY_RESPONSE_MAP: { [key: string]: string } = {
     accepted: '승인',
     rejected: '거절',
   };
 
   const handleClick = (event: React.MouseEvent) => {
     const { value } = event.target as HTMLButtonElement;
-    alert(`지원 요청을 ${applyResponseMap[value]}합니다.`);
+    alert(`지원 요청을 ${APPLY_RESPONSE_MAP[value]}합니다.`);
     const cleanUrl = removePrefix(href);
     applicationAPI.putShopApply(cleanUrl, value);
   };
@@ -25,7 +25,7 @@ function ActionButton({ href, ...rest }: ActionButtonProps) {
   return (
     <div className="flex-center gap-8px w-122px tablet:min-w-162px pc:min-w-200px ml-8px tablet:m-auto pc:m-auto">
       <button
-        className={`${styleBase} border-custom-orange text-custom-orange`}
+        className={`${BASE_STYLE} border-custom-orange text-custom-orange`}
         onClick={handleClick}
         value="rejected"
         {...rest}
@@ -33,7 +33,7 @@ function ActionButton({ href, ...rest }: ActionButtonProps) {
         거절하기
       </button>
       <button
-        className={`${styleBase} border-blue20 text-blue20`}
+        className={`${BASE_STYLE} border-blue20 text-blue20`}
         onClick={handleClick}
         value="accepted"
         {...rest}
