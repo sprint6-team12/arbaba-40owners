553189450279ea6e3a634aadaa9cd8c0b13d2b50
MountionRiver <162934516+JooKangsan@users.noreply.github.com>
2024-07-07 06:26:24 +0900

    ♻️refactor: Dd 119 refactor shop image 2차 (#105)
    * DD-119-refactor: 포맷함수 적용후 콤마찍기
    
    * DD-119-fix: 이미지 제거 적용
    
    * DD-119-refactor: baseImage 설정
    
    * DD-119-refactor: img css 수정 및 edit page 기본값 적용
    
    * DD-119-fix: edit 이미지 변경 에러 수정
    
    * DD-119-fix: 카드 작아지는 오류 수정
    
    * DD-119-fix: 파일 이미지 변경
    
    * DD-119-fix: table 깨짐 수정
    
    * DD-119-fix: Gnb 깨지는거 수정

--- src/components/Gnb/Gnb.tsx
+++ src/components/Gnb/Gnb.tsx
@@ -29,7 +29,7 @@ export default function Gnb() {
   }
 
   return (
-    <div className="flex justify-between items-center flex-wrap px-5 tablet:px-8 pc:px-[260px] py-2.5 tablet:py-4 pc:py-4">
+    <div className="flex justify-between items-center flex-wrap px-5 tablet:px-8 pc:px-[160px] py-2.5 tablet:py-4 pc:py-4">
       <Link href="/">
         <div className="flex items-center w-100px tablet:w-145px pc:w-145px h-4 tablet:h-10 pc:h-10">
           <Image src={Logo} alt="더줄게로고" />
@@ -38,7 +38,7 @@ export default function Gnb() {
       <div className="flex items-center order-2 tablet:order-none pc:order-none mt-16px tablet:mt-0 pc:mt-0">
         <SearchBar />
       </div>
-      <div className="flex items-center justify-between gap-16px tablet:gap-40px pc:gap-40px">
+      <div className="flex items-center justify-between gap-16px tablet:gap-32px pc:gap-40px">
         <GnbButton userType={type} onClick={handleGnbButtonsClick} />
         {type !== 'guest' && <NotificationButton />}
       </div>
