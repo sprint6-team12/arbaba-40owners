5b072be37d003b91f0e5a8023c79c4bb38cbc5db
김제완 <gimjewan@CarpeDiem-MacBookAir.local>
2024-09-16 03:02:32 +0900

    DD-138-fix: head-icon 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,10 +18,7 @@ export default function Document() {
           property="og:description"
           content="급하게 알바 구할 때는- 아르바바와 40인의 사장들"
         />
-        <meta
-          property="og:image"
-          content="https://cdn.discordapp.com/attachments/1228964135052644362/1259512794248904794/1.png?ex=668bf432&is=668aa2b2&hm=7f5f142140fd53d1692b6564815cbbe7caef161cb22af060af8f457c88f90e72&"
-        />
+        <meta property="og:image" content="/arbaba.png" />
         <meta property="og:url" content="https://the-julge-6-12.vercel.app/" />
         <title>아르바바와40인의사장들</title>
       </Head>
