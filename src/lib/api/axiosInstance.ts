2d04e9514fc695377d20dfde886e55d8001596f3
JooKangSan <you04045@naver.com>
2024-10-19 02:27:17 +0900

    refactor: 에러처리 및 토큰 인터셉터를 사용해 일괄처리

--- src/lib/api/axiosInstance.ts
+++ src/lib/api/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { handleAxiosError } from './ApiError';
 
 export const axiosInstance = axios.create({
   baseURL: 'https://bootcamp-api.codeit.kr/api/06-12/the-julge',
@@ -7,3 +8,22 @@ export const axiosInstance = axios.create({
   },
   withCredentials: false,
 });
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const accessToken = await localStorage.getItem('accessToken');
+    const newConfig = { ...config };
+    newConfig.headers.Authorization = `Bearer ${accessToken}`;
+    return newConfig;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const handledError = handleAxiosError(error);
+    return Promise.reject(handledError);
+  }
+);
+export default axiosInstance;
